92bb6f3d9a0ae0e52ed946a088c71236eef1c148
Veronique <veroniqueauzias@gmail.com>
2021-02-04 19:52:55 +0100

    initialize articles form

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,11 +4,13 @@
 namespace App\Controller;
 
 use App\Entity\Article;
-use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use App\Form\ArticleType;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class ArticleController extends AbstractController
 {
@@ -26,6 +28,30 @@ class ArticleController extends AbstractController
             'articles' => $articles,
         ]);
     }
+
+    /**
+     * @Route("/articles/new", name="newArticle")
+     * @return Response
+     */
+    public function new(Request $request) : Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+            return $this->redirectToRoute('articles');
+
+
+        }
+
+        return $this->render('newArticle.html.twig', [
+            "form" => $form->createView(),
+        ]);
+    }
+
 
     /**
      * @Route("/article/{id}", name="article")
---
+++ src/Form/ArticleType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('content', TextType::class)
+            ->add('picture', TextType::class)
+            ->add('category', null,
+                ['choice_label' => 'title'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
